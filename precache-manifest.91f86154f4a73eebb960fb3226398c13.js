928be584f6a060aa3de7220d7449f856d2ac4478
radoslaw.strojny <radoslaw.strojny@stxnext.pl>
2020-03-01 14:04:16 +0100

    Updates

---
+++ precache-manifest.91f86154f4a73eebb960fb3226398c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2871df067ad6ef54119c1c80007276",
+    "url": "/calc-frontend/index.html"
+  },
+  {
+    "revision": "44ddd4ca62aaa6f04ef8",
+    "url": "/calc-frontend/static/css/main.a065d8c9.chunk.css"
+  },
+  {
+    "revision": "a8db4d89e84b3b97f847",
+    "url": "/calc-frontend/static/js/2.ae769cdc.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/calc-frontend/static/js/2.ae769cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ddd4ca62aaa6f04ef8",
+    "url": "/calc-frontend/static/js/main.bba3463a.chunk.js"
+  },
+  {
+    "revision": "e65f4826efd04a8771f8",
+    "url": "/calc-frontend/static/js/runtime-main.db4f8d78.js"
+  }
+]);
